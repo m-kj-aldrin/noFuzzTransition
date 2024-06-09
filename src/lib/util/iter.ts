b795135ae79bc7652df7c031e601a494628b5650
MKJ <mkj.aldrin@icloud.com>
2024-06-09 03:10:34 +0200

    clean

--- src/lib/util/iter.ts
+++ src/lib/util/iter.ts
@@ -1,19 +1,20 @@
 type RangeFunction = {
-    <T>(size: number, fill: (index: number, array: T[]) => T): T[];
-    (size: number): undefined[];
+  <T>(size: number, fill: (index: number, array: T[]) => T): T[];
+  (size: number): undefined[];
 };
 
 export const range: RangeFunction = <T>(
-    size: number,
-    fill?: (index: number, array: T[]) => T
+  size: number,
+  fill?: (index: number, array: T[]) => T
 ): T[] | undefined[] => {
-    const result = new Array<T | undefined>(size);
-    if (fill !== undefined) {
-        for (let i = 0; i < size; i++) {
-            result[i] = fill(i, result as T[]);
-        }
-        return result as T[];
-    } else {
-        return result.fill(undefined);
+  const result = new Array<T | undefined>(size);
+  if (fill !== undefined) {
+    for (let i = 0; i < size; i++) {
+      result[i] = fill(i, result as T[]);
     }
+    return result as T[];
+  } else {
+    //@ts-ignore
+    return result.fill(undefined);
+  }
 };
