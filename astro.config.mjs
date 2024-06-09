ddb44bfcb997a0ea45ae16d18131c9632e7aa0e4
MKJ <mkj.aldrin@icloud.com>
2024-06-10 00:02:37 +0200

    y

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-    // prefetch: {
-    //     defaultStrategy: "load",
-    // },
-    // experimental: {
-    //     clientPrerender: true,
-    // },
+  prefetch: {
+    defaultStrategy: "load",
+  },
+  experimental: {
+    clientPrerender: true,
+  },
+  output: "hybrid",
+  adapter: netlify(),
 });
